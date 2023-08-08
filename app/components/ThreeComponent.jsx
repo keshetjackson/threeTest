9119343a663c60208dbd3cbe45297ee2c5ff94e3
keshet jackson <keshetjackson@gmail.com>
2023-08-08 23:48:43 +0300

    minor changes

--- app/components/ThreeComponent.jsx
+++ app/components/ThreeComponent.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { ObjectLoader } from 'three';
 import { DragControls } from 'three/examples/jsm/controls/DragControls';
+
+const canvaStyle = { height: '30rem' };
+const buttonStyle = {display: 'block', fontSize:'50px' }
 
 function useDraggable(ref, index, elements) {
   const { camera, gl } = useThree();
@@ -9,61 +12,56 @@ function useDraggable(ref, index, elements) {
   const [parentPos, setParentPos] = useState({ x: 0, y: 0, z: 0 });
 
   useEffect(() => {
-    if (ref.current) {
-      controls.current = new DragControls([ref.current], camera, gl.domElement);
+    if (!ref.current) return;
 
-      controls.current.addEventListener('drag', (event) => {
-        if (index > 0 && elements[index - 1] && elements[index - 1].current) {
-          const prevElement = elements[index - 1].current;
-          setParentPos(prevElement.position);
-          const distance = event.object.position.distanceTo(prevElement.position);
-          const maxDistance = 2; 
-
-          if (distance > maxDistance) {
-            event.object.position.set(
-              (event.object.position.x - prevElement.position.x) * maxDistance / distance + prevElement.position.x,
-              (event.object.position.y - prevElement.position.y) * maxDistance / distance + prevElement.position.y,
-              (event.object.position.z - prevElement.position.z) * maxDistance / distance + prevElement.position.z
-            );
-          }
-        } else if (index === 0) {
-          setParentPos({ x: 0, y: 0, z: 0 });
+    controls.current = new DragControls([ref.current], camera, gl.domElement);
+    controls.current.addEventListener('drag', (event) => {
+      const prevElement = elements[index - 1]?.current;
+      if (index > 0 && prevElement) {
+        setParentPos(prevElement.position);
+        const distance = event.object.position.distanceTo(prevElement.position);
+        if (distance > 2) {
+          event.object.position.set(
+            (event.object.position.x - prevElement.position.x) * 2 / distance + prevElement.position.x,
+            (event.object.position.y - prevElement.position.y) * 2 / distance + prevElement.position.y,
+            (event.object.position.z - prevElement.position.z) * 2 / distance + prevElement.position.z
+          );
         }
-      });
+      } else {
+        setParentPos({ x: 0, y: 0, z: 0 });
+      }
+    });
 
-      return () => controls.current.dispose();
-    }
+    return () => controls.current.dispose();
   }, [ref, camera, gl, index, elements]);
 
   return parentPos;
 }
 
-function Pyramid(props) {
+const Pyramid = ({ index, elements, position }) => {
   const meshRef = useRef();
-  useDraggable(meshRef, props.index, props.elements);
+  useDraggable(meshRef, index, elements);
   return (
-    <mesh ref={meshRef} position={props.position}>
+    <mesh ref={meshRef} position={position}>
       <coneGeometry args={[1, 2, 4]} />
       <meshStandardMaterial color="orange" />
     </mesh>
   );
-}
+};
 
-function Digit(props) {
+const Digit = ({ index, elements, position }) => {
   const meshRef = useRef();
-  useDraggable(meshRef, props.index, props.elements);
+  useDraggable(meshRef, index, elements);
   return (
-    <mesh ref={meshRef} position={props.position}>
+    <mesh ref={meshRef} position={position}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color="blue" />
     </mesh>
   );
-}
+};
 
-function Connector(props) {
-  const { startRef, endRef } = props;
+const Connector = ({ startRef, endRef }) => {
   const lineRef = useRef();
-
   useFrame(() => {
     if (lineRef.current && startRef.current && endRef.current) {
       const startPoint = new THREE.Vector3().copy(startRef.current.position);
@@ -79,63 +77,44 @@ function Connector(props) {
       <lineBasicMaterial color="white" />
     </line>
   );
-}
+};
+
+const ElementButton = ({ type, onAdd }) => (
+  <button style={buttonStyle} onClick={() => onAdd(type)}>Add {type.charAt(0).toUpperCase() + type.slice(1)}</button>
+);
 
 export function ThreeComponent() {
   const [elements, setElements] = useState([]);
   const elementRefs = useRef([]);
 
-  const addElement = (type) => {
-    const newElement = {
-      type: type,
-    };
-    setElements(prev => {
-      elementRefs.current.push(React.createRef());
-      return [...prev, newElement];
-    });
-  };
+  const addElement = useCallback((type) => {
+    setElements(prev => [...prev, { type }]);
+    elementRefs.current.push(React.createRef());
+  }, []);
 
-  const deleteElement = (index) => {
+  const deleteElement = useCallback((index) => {
     setElements(prev => prev.slice(0, index));
-  };
-
-
-  
-
- 
+  }, []);
 
   return (
     <div>
-      <Canvas style={{height: '30rem'}}>
+      <Canvas style={canvaStyle}>
         <ambientLight />
         <pointLight position={[100, 100, 100]} />
         {elements.map((el, index) => {
-          let element;
-          if (el.type === 'pyramid') {
-            element = <Pyramid key={index} ref={elementRefs.current[index]} index={index} elements={elementRefs.current} />;
-          } else if (el.type === 'digit') {
-            element = <Digit key={index} ref={elementRefs.current[index]} index={index} elements={elementRefs.current} />;
-          }
-
-          let connector;
-          if (index > 0 && elementRefs.current[index - 1] && elementRefs.current[index - 1].current) {
-            connector = <Connector startRef={elementRefs.current[index - 1]} endRef={elementRefs.current[index]} />;
-          }
-
+          const Element = el.type === 'pyramid' ? Pyramid : Digit;
           return (
             <React.Fragment key={index}>
-              {element}
-              {connector}
+              <Element ref={elementRefs.current[index]} index={index} elements={elementRefs.current} />
+              {index > 0 && <Connector startRef={elementRefs.current[index - 1]} endRef={elementRefs.current[index]} />}
             </React.Fragment>
           );
         })}
       </Canvas>
-      <button onClick={() => addElement('pyramid')}>Add Pyramid</button>
-      <br/>
-      <button onClick={() => addElement('digit')}>Add Digit</button>
-      <br/>
+      <ElementButton type="pyramid" onAdd={addElement} />
+      <ElementButton type="digit" onAdd={addElement} />
       {elements.map((el, index) => (
-        <button style={{display: 'block'}} key={index} onClick={() => deleteElement(index)}>Delete {el.type}</button>
+        <button style={buttonStyle} key={index} onClick={() => deleteElement(index)}>Delete {el.type}</button>
       ))}
     </div>
   );
